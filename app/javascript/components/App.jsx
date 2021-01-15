5c1be80cc3302d28a3013e4279f786a8b115b3b7
Abhishek Goswami <abhishekgoswami@Abhisheks-MacBook-Pro.local>
2021-01-15 21:28:23 +0530

    babel config working

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,14 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import TasksIndex from "./Tasks/Index";
 
 const App = (props) => {
-  useEffect(() => {
-    initializeLogger();
-    logger.info("Log from js-logger");
-  }, []);
-
   console.log("props");
   console.log(props);
   return (
