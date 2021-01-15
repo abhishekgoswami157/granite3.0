3f968f667a9d213cd38b45cd03bd9dd169ac0d78
Abhishek Goswami <abhishekgoswami@Abhisheks-MacBook-Pro.local>
2021-01-15 15:10:24 +0530

    added react router dom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
